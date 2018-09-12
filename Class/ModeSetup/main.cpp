6815efeff6581e7bd4f602f22faa848320bcd677
ml1150258 <mark.lehr@rcc.edu>
2018-09-12 14:10:26 -0700

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

--- Class/ModeSetup/main.cpp
+++ Class/ModeSetup/main.cpp
@@ -2,11 +2,13 @@
  * File:   main.cpp
  * Author: Dr. Mark E. Lehr
  * Created on September 10th, 2018, 9:07 AM
- * Purpose:  Truncation Error
+ * Purpose:  Mode Setup
  */
 
 //System Libraries Here
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 //User Libraries Here
@@ -15,25 +17,86 @@ using namespace std;
 //Like PI, e, Gravity, or conversions
 
 //Function Prototypes Here
+int *fillAry(int,int);
+void prntAry(int *,int,int);
+void swap(int &,int &);
+void smlLst(int *,int,int);
+void mrkSort(int *,int);
+void shuffle(int *,int,int);
+
 
 //Program Execution Begins Here
 int main(int argc, char** argv) {
-    //Declare all Variables and initialize Here
-    float frac=0.1;
-    float sum=0.0;
-    int nLoops=10000000;
+    //Set the random number seed
+    srand(static_cast<unsigned int>(time(0)));
     
-    //Process/Calculations Here
-    for(int loop=1;loop<=nLoops;loop++){
-        sum+=frac;
-    }
+    //Declare all Variables and initialize Here
+    int size=100;
+    int mod=10;
+    int *array=fillAry(size,mod);
+    
+    //Print the resulting array
+    prntAry(array,size,mod);
+    
+    //Shuffle the array
+    shuffle(array,size,7);
+    
+    //Print the shuffled array
+    prntAry(array,size,mod);
+    
+    //Sort the array
+    mrkSort(array,size);
     
     //Output Located Here
-    cout<<"Answer should be =>"<<frac<<"*"
-            <<nLoops<<"="<<frac*nLoops<<endl;
-    cout<<"Actual answer = "<<sum<<endl;
-    cout<<"Error = "<<(1-sum/frac/nLoops)*100<<"%"<<endl;
+    prntAry(array,size,mod);
+    
+    //Clean Memory
+    delete []array;
 
     //Exit
     return 0;
+}
+
+void shuffle(int *a,int n,int nShuf){
+    for(int shuf=1;shuf<=nShuf;shuf++){
+        for(int i=0;i<n;i++){
+            int temp=rand()%n;
+            if(i!=temp)swap(a[i],a[temp]);
+        }
+    }
+}
+
+void mrkSort(int *a,int n){
+    for(int i=0;i<n-1;i++){
+        smlLst(a,n,i);
+    }
+}
+
+void smlLst(int *a,int n,int pos){
+    for(int i=pos+1;i<n;i++){
+        if(a[pos]>a[i])swap(a[pos],a[i]);
+    }
+}
+
+void swap(int &a,int &b){
+    a=a^b;
+    b=a^b;
+    a=a^b;
+}
+
+void prntAry(int *a,int n,int perLine){
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+        if(i%perLine==(perLine-1))cout<<endl;
+    }
+    cout<<endl;
+}
+
+int *fillAry(int n,int m){
+    int *array=new int[n];
+    for(int i=0;i<n;i++){
+        array[i]=i%m;
+    }
+    return array;
 }
